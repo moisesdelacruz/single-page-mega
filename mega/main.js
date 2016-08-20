e704ad44a928fa6a0c9d7a9061a097b977d60255
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-08-20 03:15:03 -0400

    MEGA. Effect of bar search; buttons products, and import button.

--- mega/main.js
+++ mega/main.js
@@ -1,5 +1,9 @@
 $(function() {
   $('.search').on('click', (e) => {
+    e.stopPropagation()
     $('.content-search').css('width', '200px')
+  })
+  $('body').on('click', (e) => {
+    $('.content-search').css('width', '100px')
   })
 })
