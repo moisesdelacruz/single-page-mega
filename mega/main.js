c96687b343a464dcc6d44e119266d7ada0c53e25
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-08-24 02:25:05 -0400

    navigation menu completo, content download (info-file and buttons of download).

--- mega/main.js
+++ mega/main.js
@@ -4,6 +4,31 @@ $(function() {
     $('.content-search').css('width', '200px')
   })
   $('body').on('click', (e) => {
+    // hide search
     $('.content-search').css('width', '100px')
+  })
+
+  // Menu
+  $('.btn-menu').on('click', (e) => {
+    e.stopPropagation()
+    $('.menu').slideToggle()
+  })
+
+  // Submenu
+  $('.btn-submenu').on('mouseover', (e) => {
+    e.stopPropagation()
+    var $submenu = $(e.target).children('.submenu')
+    $($submenu).css({
+      display: 'block',
+      right: '202px'
+    })
+  })
+  $('.btn-submenu').on('mouseout', (e) => {
+    e.stopPropagation()
+    var $submenu = $(e.target).children('.submenu')
+    $($submenu).css({
+      right: '0',
+      display: 'none'
+    })
   })
 })
